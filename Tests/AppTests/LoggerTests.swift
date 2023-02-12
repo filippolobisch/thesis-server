c5f505c3091e8f3e78bfc9fd109762c7013c1e95
Filippo Lobisch <filippolobisch@icloud.com>
2023-02-12 18:43:27 +0200

    Simple logger object to log when adaptations are performed plus its test

---
+++ Tests/AppTests/LoggerTests.swift
@@ -0,0 +1,23 @@
+//
+//  LoggerTests.swift
+//  
+//
+//  Created by Filippo Lobisch on 12.02.23.
+//
+
+import Foundation
+import XCTest
+@testable import App
+
+/// The test class that handles all the test cases for the `Logger` object.
+final class LoggerTests: XCTestCase {
+    /// Tests the addition of a message ot the logs.
+    func testAddLogMessage() throws {
+        let logger = Logger()
+        let logMessage = "This is the expected log message."
+        logger.add(message: logMessage)
+        let expectation = "\(Date())\t\(logMessage)"
+        
+        XCTAssertEqual(logger.logs, expectation, "The retrieved log was expected to be equal to the expectation, however, they do not equate.")
+    }
+}
