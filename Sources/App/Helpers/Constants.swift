9d9082a1288654aa4fa968cba16088f9bc7af480
Filippo Lobisch <filippolobisch@icloud.com>
2023-02-15 18:00:44 +0200

    Added constants file so bucket names do not need to be typed many times. This prevents spelling errors that cause the application to not function as we want.

---
+++ Sources/App/Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Filippo Lobisch on 28.01.23.
+//
+
+import Foundation
+
+enum Constants: String {
+    case euS3BucketName = "eu-data-bucket"
+    case naS3BucketName = "na-data-bucket"
+}
