0b44e242ca2ac2070a0e9628ae4a0c5b3ece51c8
Filippo Lobisch <filippolobisch@icloud.com>
2023-02-23 11:08:56 +0200

    Updated tests to not recreate an object each time a test case is executing.

--- Tests/AppTests/AWSManagerTests.swift
+++ Tests/AppTests/AWSManagerTests.swift
@@ -11,32 +11,31 @@ import XCTest
 
 /// The test class that handles all the test cases for the `AWSManager` object.
 final class AWSManagerTests: XCTestCase {
-    /// The bucket name of the AWS S3 storage bucket to be tested on.
-    private let bucketName = "eu-data-bucket"
+    
+    /// The main awsManager property that is used in all the tests.
+    /// We create it here so that it does not get recreated for each test.
+    private let awsManager = AWSS3Manager(bucketName: "eu-data-bucket", region: .euCentral1)
     
     /// Test the retrieval of all objects in an AWS Bucket.
     func testListObjectsEU() async throws {
-        let awsManager = AWSS3Manager(bucketName: bucketName, region: .euCentral1)
         let files = try await awsManager.getAllFilesInBucket()
         XCTAssertTrue(files.count >= 0, "The number of retrieved files from the aws bucket was expected to be greater or equal to 0, however, the value was less than 0.")
     }
     
     /// Test the upload of data to the AWS bucket.
     func testUploadToBucket() async throws {
-        let awsManager = AWSS3Manager(bucketName: bucketName, region: .euCentral1)
         let filename = "test-upload-data-file-name"
         let fileData = Data("This is the test data from the testUploadData test case in the AWSManagerTests.".utf8)
         let didUploadFile = try await awsManager.upload(data: fileData, using: filename)
         XCTAssertTrue(didUploadFile, "Uploading the file to AWS Bucket expected to be true, however, the file is not included in the bucket files.")
             
         addTeardownBlock {
-            _ = try await awsManager.delete(fileKey: filename)
+            _ = try await self.awsManager.delete(fileKey: filename)
         }
     }
     
     /// Test the download of file from the AWS bucket.
     func testDownloadFile() async throws {
-        let awsManager = AWSS3Manager(bucketName: bucketName, region: .euCentral1)
         let filename = "Latex Cache.md"
         let expectedContent = Data("# Fix BibTex cache\n\nFirst check it exists with: `biber --cache`\n\nTo clear cache: `rm -rf `biber --cache``\n".utf8)
         
@@ -46,7 +45,6 @@ final class AWSManagerTests: XCTestCase {
     
     /// Test the delete of a file in the AWS bucket.
     func testDeleteFile() async throws {
-        let awsManager = AWSS3Manager(bucketName: bucketName, region: .euCentral1)
         let filename = "test-delete-file-name"
         let fileData = Data("This is the test data from the testDeleteFile test case in the AWSManagerTests.".utf8)
         _ = try await awsManager.upload(data: fileData, using: filename)
--- Tests/AppTests/OutsideEUTests.swift
+++ Tests/AppTests/OutsideEUTests.swift
@@ -1,6 +1,6 @@
 //
 //  OutsideEUTests.swift
-//  
+//
 //
 //  Created by Filippo Lobisch on 22.02.23.
 //
@@ -12,10 +12,13 @@ import XCTest
 /// The test class that handles all the test cases for the `OutsideEU` object.
 final class OutsideEUTests: XCTestCase {
     
+    /// The main outsideEU property that is used in all the tests.
+    /// We create it here so that it does not get recreated for each test.
+    private let outsideEU = OutsideEU()
+    
     /// Tests the creation of a `Task` object that continuously runs.
     /// We check to ensure that the `task` property is not nil after its creation inside the `getFilesConstantly` method.
     func testTaskCreation() {
-        let outsideEU = OutsideEU()
         outsideEU.getFilesConstantly()
         
         XCTAssertNotNil(outsideEU.task, "Expected the task to not be nil, however, the task was nil.")
@@ -24,7 +27,6 @@ final class OutsideEUTests: XCTestCase {
     /// Tests the cancellation of a `Task` object that continuously runs.
     /// We check to ensure that the `task` property is nil after its cancellation inside the `cancelTask` method, signifying the task was successfully cancelled.
     func testTaskCancellation() async throws {
-        let outsideEU = OutsideEU()
         outsideEU.getFilesConstantly()
         outsideEU.cancelTask()
         
@@ -36,7 +38,6 @@ final class OutsideEUTests: XCTestCase {
     /// We also ensure that the correct changes to the system are performed (removing content from north american bucket), to verify that the adaptation executes as we expect.
     /// Lastly we reset the changes that were made to the system. Since we are dealing with a single file we upload that file to the AWS bucket.
     func testOddNumberOfTimesToExecuteAdaptation() async throws {
-        let outsideEU = OutsideEU()
         XCTAssertFalse(outsideEU.storeDataOnlyInEU, "Expected 'storeDataOnlyInEU' property to be false when starting the execution, however, the property is true.")
         
         _ = try await outsideEU.executeAdaptation(model: "", numberOfTimesToExecute: 1)
@@ -48,7 +49,7 @@ final class OutsideEUTests: XCTestCase {
         XCTAssertTrue(files.isEmpty, "Expected files in North American bucket to be empty, however, files still exist.")
 
         addTeardownBlock {
-            _ = try await outsideEU.northAmericaAWSManager.upload(resource: "Latex Cache", withExtension: "md")
+            _ = try await self.outsideEU.northAmericaAWSManager.upload(resource: "Latex Cache", withExtension: "md")
         }
     }
     
@@ -56,7 +57,6 @@ final class OutsideEUTests: XCTestCase {
     /// We ensure that there is no change in the `storeDataOnlyInEU` property (stays false).
     /// We also ensure that there are no changes to the system are performed ie content remains where it was previous to this adaptation call, to verify that the adaptation executes as we expect.
     func testEvenNumberOfTimesToExecuteAdaptation() async throws {
-        let outsideEU = OutsideEU()
         XCTAssertFalse(outsideEU.storeDataOnlyInEU, "Expected 'storeDataOnlyInEU' property to be false when starting the execution, however, the property is true.")
         
         _ = try await outsideEU.executeAdaptation(model: "", numberOfTimesToExecute: 2)
