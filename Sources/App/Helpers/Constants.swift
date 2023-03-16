16601f435fcef4455df7f8b93771b28c4ce311a2
Filippo Lobisch <filippolobisch@icloud.com>
2023-03-16 11:17:55 +0200

    Updated stress test http route and added documentation to methods

--- Sources/App/Helpers/Constants.swift
+++ Sources/App/Helpers/Constants.swift
@@ -1,12 +1,14 @@
 //
 //  Constants.swift
-//  
+//
 //
 //  Created by Filippo Lobisch on 28.01.23.
 //
 
 import Foundation
 
+/// Useful constants for this project.
+/// We use this so that we can call `Constants.euS3BucketName.rawValue` each time to ensure no spelling mistakes occur if we need to type the bucket name.
 enum Constants: String {
     case euS3BucketName = "eu-data-bucket"
     case naS3BucketName = "fl-na-data-bucket"
