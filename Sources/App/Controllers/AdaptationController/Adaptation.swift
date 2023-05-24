5b9c9a9783047b37872294e2be1cbd51a32d8403
Filippo Lobisch <filippolobisch@icloud.com>
2023-05-24 18:40:48 +0300

    Add adaptation protocol

---
+++ Sources/App/Controllers/AdaptationController/Adaptation.swift
@@ -0,0 +1,29 @@
+//
+//  Adaptation.swift
+//  
+//
+//  Created by Filippo Lobisch on 23.05.23.
+//
+
+import Foundation
+
+enum AdaptationType: Int {
+    case outsideEU = 1
+    case sensitiveData = 2
+    
+    init(_ string: String) {
+        guard let key = Int(string), let adaptation = AdaptationType(rawValue: key) else {
+            fatalError("The returned key does not contain an appropriate adaptation key.")
+        }
+        
+        self = adaptation
+    }
+}
+
+
+protocol Adaptation {
+    var useComponentB: Bool { get }
+    var componentB: ComponentBController { get }
+    
+    mutating func executeAdaptation() async throws -> Bool
+}
